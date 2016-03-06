2124bbe8920300485e6466f9f10c5b5444504672
The Boss <JanatiCorp.@Alis-MacBook-Air.local>
2016-03-05 20:18:30 -0500

    gif

---
+++ TwitterDemo/TweetsCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetsCell.swift
+//  TwitterDemo
+//
+//  Created by The Boss on 3/5/16.
+//  Copyright © 2016 The Boss. All rights reserved.
+//
+
+import UIKit
+
+class TweetsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
